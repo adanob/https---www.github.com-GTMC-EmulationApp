5091ce17f45410ddc3bab40a9ed12e3a1bdd9637
Adano <a.brown@chargebacks911.com>
2026-02-25 00:36:48 -0500

    Update emulation.php

--- ui/config/emulation.php
+++ ui/config/emulation.php
@@ -6,15 +6,13 @@ return [
     | EmulationApp Root Path
     |--------------------------------------------------------------------------
     |
-    | Absolute path to the EmulationApp directory where engine/, payloads/,
-    | scripts/, and .emulation_key live.
+    | Absolute path to the EmulationApp directory where engine/, jobs/,
+    | and .emulation_key live.
     |
     | On your machine this is likely: C:\wamp64\www\EmulationApp
     |
     */
     'app_root' => env('EMULATION_APP_ROOT', base_path('../')),
 
-    'payloads_dir' => env('EMULATION_PAYLOADS_DIR', base_path('../payloads')),
-
-    'scripts_dir' => env('EMULATION_SCRIPTS_DIR', base_path('../scripts')),
+    'jobs_dir' => env('EMULATION_JOBS_DIR', base_path('../jobs')),
 ];
