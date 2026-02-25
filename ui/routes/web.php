342815aa2a97c55aa4c2c375f7c1780b145b26f2
Adano <a.brown@chargebacks911.com>
2026-02-24 19:48:13 -0500

    Update web.php

--- ui/routes/web.php
+++ ui/routes/web.php
@@ -3,7 +3,10 @@
 use App\Http\Controllers\EmulationController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',               [EmulationController::class, 'index']);
-Route::post('/payload',       [EmulationController::class, 'store'])->name('payload.store');
-Route::get('/payload/{name}', [EmulationController::class, 'show'])->name('payload.show');
-Route::delete('/payload/{name}', [EmulationController::class, 'destroy'])->name('payload.destroy');
+Route::get('/',                     [EmulationController::class, 'index']);
+Route::post('/payload',             [EmulationController::class, 'store'])->name('payload.store');
+Route::post('/payload/upload',      [EmulationController::class, 'upload'])->name('payload.upload');
+Route::get('/payload/{name}',       [EmulationController::class, 'show'])->name('payload.show');
+Route::delete('/payload/{name}',    [EmulationController::class, 'destroy'])->name('payload.destroy');
+Route::post('/payload/{name}/run',  [EmulationController::class, 'run'])->name('payload.run');
+Route::post('/settings',            [EmulationController::class, 'saveSettings'])->name('settings.save');
