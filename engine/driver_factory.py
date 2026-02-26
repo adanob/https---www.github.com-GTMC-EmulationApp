dfb911e2441cb929f6164c145a75b4b3c99f2d0f
Adano <a.brown@chargebacks911.com>
2026-02-26 02:00:01 -0500

    Update driver_factory.py

--- engine/driver_factory.py
+++ engine/driver_factory.py
@@ -55,12 +55,9 @@ class DriverFactory:
             options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
         options.add_argument("--start-maximized")
         options.add_argument("--window-size=1920,1080")
-
-        # Only disable GPU on headless (causes visibility issues on Windows desktop)
-        if headless:
-            options.add_argument("--disable-gpu")
 
         # Configure downloads
         prefs = {
