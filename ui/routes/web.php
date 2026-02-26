3e25e01390779bab4a9c4fff5c790949f7e37c71
Adano <a.brown@chargebacks911.com>
2026-02-25 21:17:51 -0500

    Update web.php

--- ui/routes/web.php
+++ ui/routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',                     [EmulationController::class, 'index']);
 Route::post('/payload',             [EmulationController::class, 'store'])->name('payload.store');
 Route::post('/payload/upload',      [EmulationController::class, 'upload'])->name('payload.upload');
+Route::get('/script/{name}/tokens', [EmulationController::class, 'scriptTokens'])->name('script.tokens');
 Route::get('/payload/{name}',       [EmulationController::class, 'show'])->name('payload.show');
 Route::delete('/payload/{name}',    [EmulationController::class, 'destroy'])->name('payload.destroy');
 Route::post('/payload/{name}/run',  [EmulationController::class, 'run'])->name('payload.run');
